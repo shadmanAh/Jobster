72a0502ee8cd5bc66e24ab35901289febbc70554
shadmanAh <shadman061@gmail.com>
2023-08-19 20:43:26 +0330

    complite the Profile and update Profile && add userSlice.js

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,6 +6,7 @@ import {
   getUserFromLocalStorage,
   removeUserFromLocalStorage,
 } from '../../utils/localStorage'
+import { loginUserThunk, registerUserThunk, updateUserThunk } from './userThunk'
 
 const initialState = {
   isLoading: false,
@@ -16,24 +17,21 @@ const initialState = {
 export const registerUser = createAsyncThunk(
   'user/registerUser',
   async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.post('/auth/register', user)
-      return resp.data
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg)
-    }
+    return registerUserThunk('/auth/register', user, thunkAPI)
   }
 )
 
 export const loginUser = createAsyncThunk(
   'user/loginUser',
   async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.post('/auth/login', user)
-      return resp.data
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg)
-    }
+    return loginUserThunk('/auth/login', user, thunkAPI)
+  }
+)
+
+export const updateUser = createAsyncThunk(
+  'user/updateUser',
+  async (user, thunkAPI) => {
+    return updateUserThunk('/auth/updateUser', user, thunkAPI)
   }
 )
 
@@ -76,6 +74,20 @@ const userSlice = createSlice({
       toast.success(`Welcome Back ${user.name}`)
     },
     [loginUser.rejected]: (state, { payload }) => {
+      state.isLoading = false
+      toast.error(payload)
+    },
+    [updateUser.pending]: (state) => {
+      state.isLoading = true
+    },
+    [updateUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload
+      state.isLoading = false
+      state.user = user
+      addUserToLocalStorage(user)
+      toast.success(`User Updated!`)
+    },
+    [updateUser.rejected]: (state, { payload }) => {
       state.isLoading = false
       toast.error(payload)
     },
--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -1,7 +1,74 @@
 import React from 'react'
+import { useState } from 'react'
+import { FormRow } from '../../components'
+import Wrapper from '../../assets/wrappers/DashboardFormPage'
+import { useDispatch, useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
+import { updateUser } from '../../features/user/userSlice'
 
 function Profile() {
-  return <div>Profile</div>
+  const { isLoading, user } = useSelector((store) => store.user)
+  const dispatch = useDispatch()
+  const [userDate, setUserDate] = useState({
+    name: user?.name || '',
+    email: user?.email || '',
+    lastName: user?.lastName || '',
+    location: user?.location || '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { name, email, lastName, location } = userDate
+    if (!name || !email || !lastName || !location) {
+      toast.error('please fill out all fields')
+      return
+    }
+    dispatch(updateUser(userDate))
+  }
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setUserDate({ ...userDate, [name]: value })
+  }
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>Profile</h3>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="name"
+            value={userDate.name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            labelText="last name"
+            name="lastName"
+            value={userDate.lastName}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={userDate.email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            name="location"
+            value={userDate.location}
+            handleChange={handleChange}
+          />
+          <button type="submit" className="btn btn-block" disabled={isLoading}>
+            {isLoading ? 'Please Wait...' : 'save changes'}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  )
 }
 
 export default Profile
