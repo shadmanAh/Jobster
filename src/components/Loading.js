6c5f73072fabefd0f53de168e952275afc923517
shadmanAh <shadman061@gmail.com>
2023-08-21 19:15:01 +0330

    create AllJobs and contents

---
+++ src/components/Loading.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Loading({ center }) {
+  return <div className={center ? 'loading loading-center' : 'loading'}></div>
+}
+
+export default Loading
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,16 @@ import Navbar from './Navbar'
 import SmallSidebar from './SmallSidebar'
 import BigSidebar from './BigSidebar'
 import FormRowSelect from './FormRowSelect'
+import SearchContainer from './SearchContainer'
+import JobsContainer from './JobsContainer'
 
-export { Logo, FormRow, Navbar, SmallSidebar, BigSidebar, FormRowSelect }
+export {
+  Logo,
+  FormRow,
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+  FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+}
